33bebbca947c3b610b9a86125a3e0ec5b0e6af12
Garima Paudel <paudelgarima229@gmail.com>
2025-05-28 10:05:36 +0545

    Minor changes

--- app/database/connection.py
+++ app/database/connection.py
@@ -8,7 +8,6 @@ def connect_to_mongo():
     Connect to MongoDB using the provided URI.
     """
 
-    # uri = "mongodb+srv://garima:garima@cluster0.5goe2sc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"  
     uri = settings.MONGODB_URI 
     # Create a new client and connect to the server
     client = MongoClient(uri, server_api=ServerApi('1'))
